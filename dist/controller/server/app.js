adb515d1217975f0f6db17b2dcc4c5a7f5425411
Renato Oliveira <renato.loliveira89@gmail.com>
2023-10-28 07:07:49 -0300

    adicionando comentários app.ts
    adicionando comentários no app.ts e gerando os novos arquivos .js

--- dist/controller/server/app.js
+++ dist/controller/server/app.js
@@ -12,17 +12,25 @@ const app = (0, express_1.default)();
 // Definindo a porta onde o servidor vai
 // escutar as requisições
 const port = 3000;
+// configurando o servidor para usar JSON
 app.use(express_1.default.json());
+// Configurando o servidor para usar o CORS
 app.use((0, cors_1.default)());
 // Rota principal do servidor
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+// Rota de cadastro de usuário
 app.post('/user', (req, res) => {
+    // exibe os dados recebidos na requisição
     console.log('Solicitação recebida:', req.body);
+    // recupera os dados recebidos na requisição
     const { nome, cpf, data_nascimento, telefone, endereco, altura, peso } = req.body;
+    // instancia um novo objeto usando os dados da requisição
     const novaPessoa = new Pessoa_1.Pessoa(nome, cpf, new Date(data_nascimento), telefone, endereco, altura, peso);
+    // persiste o objeto instanciado no banco de dados
     console.log((0, bancoDeDados_1.persistir)(novaPessoa));
+    // retorna uma reposta ao front-end
     res.json({ mensagem: 'Dados recebidos com sucesso!' });
 });
 // Executando o servidor
--- src/controller/server/app.ts
+++ src/controller/server/app.ts
@@ -9,8 +9,10 @@ const app = express();
 // escutar as requisições
 const port: number = 3000;
 
+// configurando o servidor para usar JSON
 app.use(express.json());
 
+// Configurando o servidor para usar o CORS
 app.use(cors());
 
 // Rota principal do servidor
@@ -18,16 +20,22 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Rota de cadastro de usuário
 app.post('/user', (req, res) => {
 
+  // exibe os dados recebidos na requisição
   console.log('Solicitação recebida:', req.body);
 
+  // recupera os dados recebidos na requisição
   const { nome, cpf, data_nascimento, telefone, endereco, altura, peso } = req.body;
 
+  // instancia um novo objeto usando os dados da requisição
   const novaPessoa = new Pessoa(nome, cpf, new Date(data_nascimento), telefone, endereco, altura, peso);
 
+  // persiste o objeto instanciado no banco de dados
   console.log(persistir(novaPessoa));
 
+  // retorna uma reposta ao front-end
   res.json({ mensagem: 'Dados recebidos com sucesso!' });
 });
 
